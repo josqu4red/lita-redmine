737ed2f9ebd8af6228e6a3c3fe62dee0cb50654f
Jonathan Amiez <jonathan.amiez@gmail.com>
2014-02-24 15:28:44 +0100

    add 2 test cases

--- spec/lita/handlers/redmine_spec.rb
+++ spec/lita/handlers/redmine_spec.rb
@@ -1,4 +1,6 @@
 require "spec_helper"
 
 describe Lita::Handlers::Redmine, lita_handler: true do
+  it { routes("get me redmine 12345, please").to(:redmine) }
+  it { doesnt_route("redmine foo").to(:redmine) }
 end
